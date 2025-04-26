a2612e318f43aca1e7fe4eb6fc88ec031a2a078b
Jiyu215 <jinyu010512@gmail.com>
2025-04-26 18:15:25 +0900

    테스트

--- src/pages/VideoCallRoom/VideoRoom.jsx
+++ src/pages/VideoCallRoom/VideoRoom.jsx
@@ -233,7 +233,7 @@ const VideoRoom = () =>{
         setListOpen(!listOpen);
     }
     
-    const wsServerUrl = "wss://vtestmo.kro.kr/call";
+    const wsServerUrl = "wss://vmotest.kro.kr/call";
     const ws = useRef(null);  // 웹소켓 연결을 위한 ref    
 
     const handleUserDataChange = (data) => {
